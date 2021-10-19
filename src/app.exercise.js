78e5ee22a95585a89a659e3e9e559142af816dbb
Alena Katkova <katkova.web@gmail.com>
2021-10-19 18:46:54 +0200

    stay logged in on refresh

--- src/app.exercise.js
+++ src/app.exercise.js
@@ -1,35 +1,40 @@
 /** @jsx jsx */
 import {jsx} from '@emotion/core'
-
 import * as React from 'react'
 import * as auth from 'auth-provider'
+import {client} from 'utils/api-client'
 import {AuthenticatedApp} from './authenticated-app'
 import {UnauthenticatedApp} from './unauthenticated-app'
 
-function App() {
-  const [user, setUser] = React.useState(null);
-
-  const login = (formData) => {
-    return auth.login(formData).then(resolvedUser => setUser(resolvedUser));
-  };
-
-  const register = (formData) => {
-   return auth.register(formData).then(resolvedUser => setUser(resolvedUser));
-  };
-
-  const logout = () => {
-    auth.logout();
-    setUser(null);
-  };
-
+const getUser = async () => {
+  let user = null
+  const token = await auth.getToken()
+  if (token) {
+    const data = await client('me', {token})
+    user = data.user
+  }
   return user
-    ? <AuthenticatedApp user={user} logout={logout} />
-    : <UnauthenticatedApp login={login} register={register}/>
+}
+
+function App() {
+  const [user, setUser] = React.useState(null)
+
+  const login = form => auth.login(form).then(u => setUser(u))
+  const register = form => auth.register(form).then(u => setUser(u))
+  const logout = () => {
+    auth.logout()
+    setUser(null)
+  }
+
+  React.useEffect(() => {
+    getUser().then((u) => setUser(u))
+  }, [])
+
+  return user ? (
+    <AuthenticatedApp user={user} logout={logout} />
+  ) : (
+    <UnauthenticatedApp login={login} register={register} />
+  )
 }
 
 export {App}
-
-/*
-eslint
-  no-unused-vars: "off",
-*/
