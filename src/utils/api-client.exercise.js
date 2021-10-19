4ec12adaa41cd9c764f9a25b7c873ef02786dcd3
Alena Katkova <katkova.web@gmail.com>
2021-10-19 19:35:26 +0200

    add post method to queries

--- src/utils/api-client.exercise.js
+++ src/utils/api-client.exercise.js
@@ -1,14 +1,16 @@
-import {logout} from '../auth-provider'
-
+import * as auth from 'auth-provider'
 const apiURL = process.env.REACT_APP_API_URL
 
 function client(
   endpoint,
-  {token, headers: customHeaders, ...customConfig} = {},
+  {data, token, headers: customHeaders, ...customConfig} = {},
 ) {
   const config = {
+    method: data ? 'POST' : 'GET',
+    body: data ? JSON.stringify(data) : undefined,
     headers: {
       Authorization: token ? `Bearer ${token}` : undefined,
+      'Content-Type': data ? 'application/json' : undefined,
       ...customHeaders,
     },
     ...customConfig,
@@ -16,11 +18,11 @@ function client(
 
   return window.fetch(`${apiURL}/${endpoint}`, config).then(async response => {
     if (response.status === 401) {
-      await logout();
+      await auth.logout()
+      // refresh the page for them
       window.location.assign(window.location)
-      return Promise.reject({message: 'authentication required'})
+      return Promise.reject({message: 'Please re-authenticate.'})
     }
-
     const data = await response.json()
     if (response.ok) {
       return data
